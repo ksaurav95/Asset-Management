eb965c1550893c47dbb2f121f5957800ec927e23
ksaurav95 <65075723+ksaurav95@users.noreply.github.com>
2022-03-09 19:36:00 +0530

    Create AccountException.java

---
+++ src/main/java/com/db/awmd/challenge/exception/AccountException.java
@@ -0,0 +1,24 @@
+package com.db.awmd.challenge.exception;
+
+@SuppressWarnings("serial")
+public class AccountException extends RuntimeException {
+
+	public AccountException() {
+	}
+
+	public AccountException(String message) {
+		super(message);
+	}
+
+	public AccountException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public AccountException(Throwable cause) {
+		super(cause);
+	}
+
+	public AccountException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+}
