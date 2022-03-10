c878c0b01f2dedcd2be7cf592eb4b5378c2e4d64
ksaurav95 <65075723+ksaurav95@users.noreply.github.com>
2022-03-10 09:52:30 +0530

    Create AccountTransferBalanceRequest.java

---
+++ src/main/java/com/db/awmd/challenge/web/dto/AccountTransferBalanceRequest.java
@@ -0,0 +1,24 @@
+package com.db.awmd.challenge.web.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@Data
+public class AccountTransferBalanceRequest {
+	@NotNull
+	private String receiverId;
+	@NotNull
+	@Min(value = 0, message = "Transfer amount must be positive.")
+	private BigDecimal amount;
+
+	public String getReceiverId() {
+		return receiverId;
+	}
+
+	public BigDecimal getAmount() {
+		return amount;
+	}
+}
